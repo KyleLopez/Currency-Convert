cdea1ea21e55a0fc87913be58da2533d803a4d3a
Kyle Lopez <talila@KyleMini.local>
2013-03-11 16:29:44 -0700

    Initial Commit

---
+++ Currency Convert/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Currency Convert
+//
+//  Created by Kyle Lopez on 3/11/13.
+//  Copyright (c) 2013 Kyle Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Currency Convert/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Currency Convert
+//
+//  Created by Kyle Lopez on 3/11/13.
+//  Copyright (c) 2013 Kyle Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
